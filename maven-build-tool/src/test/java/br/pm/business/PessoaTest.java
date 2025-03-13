7bbffee81ffe28bf177b75acab6e6fad54ed2297
Laura Pontara <lauracplopes@gmail.com>
2025-03-13 11:22:23 -0300

    Correções na estrutura do projeto

---
+++ maven-build-tool/src/test/java/br/pm/business/PessoaTest.java
@@ -0,0 +1,30 @@
+package br.pm.business;
+
+import org.junit.jupiter.api.Test;
+
+public class PessoaTest {
+    @Test
+    void testMaiorIdade() {
+
+    }
+
+    @Test
+    void testSetCpf() {
+
+    }
+
+    @Test
+    void testSetIdade() {
+
+    }
+
+    @Test
+    void testSetNome() {
+
+    }
+
+    @Test
+    void testSetSexo() {
+
+    }
+}
